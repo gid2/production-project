fa3b2cc53ad4e1da8cd78549dee4e418358d90b5
gid2 <103135483+gid2@users.noreply.github.com>
2024-03-07 19:56:50 +0300

    lesson-29: add modalWindow and portal

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,18 +1,19 @@
-import React, {
-    FC, ReactNode, useMemo, useState,
-} from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
 interface ThemeProviderProps {
-    children: ReactNode; // Типизация свойства children
     initialTheme?: Theme;
-  }
+}
 
-// eslint-disable-next-line max-len
-const ThemeProvider: FC<React.PropsWithChildren<ThemeProviderProps>> = ({ children, initialTheme }) => {
-    const [theme, setTheme] = useState<Theme>(defaultTheme);
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+    const {
+        initialTheme,
+        children,
+    } = props;
+
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
     const defaultProps = useMemo(() => ({
         theme,
