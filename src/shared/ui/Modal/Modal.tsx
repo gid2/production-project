544857639a3699e11fb178c75ce29937423a48e1
gid2 <103135483+gid2@users.noreply.github.com>
2024-03-09 12:32:58 +0300

    fix theme for modal window

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -64,7 +64,7 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div
                     className={cls.overlay}
                     onClick={closeHandler}
