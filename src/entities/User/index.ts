5f3b7e5cd73bf0af0e41a12c71529188807aa6fa
gid2 <103135483+gid2@users.noreply.github.com>
2024-03-13 15:07:24 +0300

    Lesson-34: add login module

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,9 +1,11 @@
-import { UserSchema, User } from './model/types/user';
 import { userReducer, userActions } from './model/slice/userSlice';
+import { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+import { UserSchema, User } from './model/types/user';
 
 export {
     userReducer,
     userActions,
     UserSchema,
     User,
+    getUserAuthData,
 };
--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,5 +1,9 @@
 import { LoginModal } from 'features/AuthByUsername/ui/LoginModal/LoginModal';
+import { LoginSchema } from 'features/AuthByUsername/model/types/loginSchema';
+import { loginReducer } from './model/slice/loginSlice';
 
 export {
     LoginModal,
+    LoginSchema,
+    loginReducer,
 };
