adebdf5a077faf876191f2f60d5a438f4876947c
gidgendugov <shinigami0202@mail.ru>
2024-10-20 16:37:39 +0300

    add jest-html-reports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -44,6 +44,15 @@ const config: Config = {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
 
 };
 
